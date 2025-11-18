f99eead2a628e59962d1fc969dae44a576915e2c
Christoper Melis <christophermx95@gmail.com>
2025-11-18 11:51:33 +0100

    v1.0.4 react version

--- react/Portfolio/src/app/routes/LanguageRedirect.tsx
+++ react/Portfolio/src/app/routes/LanguageRedirect.tsx
@@ -1,5 +1,6 @@
 import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
+import i18n from "i18next";
 
 export default function LanguageRedirect() {
     const navigate = useNavigate();
@@ -11,6 +12,7 @@ export default function LanguageRedirect() {
 
         const supported = ["fr", "en", "nl"];
         const finalLang = supported.includes(lang) ? lang : "en";
+        i18n.changeLanguage(finalLang);
 
         navigate(`/intro/${finalLang}`, { replace: true });
     }, []);
